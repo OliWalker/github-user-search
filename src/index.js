aff2cd19f85f0088a77298f0dd947aea6cf058c6
oliwalker <ofwalker@live.co.uk>
2019-01-29 22:36:42 +0100

    added .env

--- src/index.js
+++ src/index.js
@@ -2,18 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider } from "react-apollo";
 import { InMemoryCache } from 'apollo-cache-inmemory';
-//import { ENDPOINT, TOKEN } from './configure'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import ApolloClient from "apollo-boost";
 
-const cache = new InMemoryCache();
+const TOKEN = process.env.REACT_APP_TOKEN;
+const ENDPOINT = process.env.REACT_APP_GITHUB_ENDPOINT;
 
+const cache = new InMemoryCache();
 const client = new ApolloClient({
-  uri: process.env.REACT_APP_GITHUB_ENDPOINT,
+  uri: ENDPOINT,
   headers: {
-    'Authorization': `Bearer ${process.env.REACT_APP_TOKEN}`,
+    'Authorization': `Bearer ${TOKEN}`,
   },
   cache
 });
